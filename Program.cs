50fafdf517645e944d02b326ed3969f0ae28e190
sevstar2016 <45399728+sevstar2016@users.noreply.github.com>
2021-03-15 11:41:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+class Program
+{
+     private static TelegramBotClient client;
+     static void Main(string[] args)
+     {
+         client = new TelegramBotClient("1615771001:AAF522v6iLHQBChZdvx-kTJ9mmxg6aubor4"); 
+         client.OnMessage += BotOnMessageReceived;
+         client.OnMessageEdited += BotOnMessageReceived;
+         client.StartReceiving();
+         Console.ReadLine();
+         client.StopReceiving();
+     }
+     private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+     {
+        var message = messageEventArgs.Message;
+        if(message?.Type == MessageType.Text)
+        {
+            if(message.Text == "ping")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, "pong");
+            }
+        }
+     }
+}
